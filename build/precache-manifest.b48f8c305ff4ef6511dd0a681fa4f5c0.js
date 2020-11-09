c9c269d326cf8c61cb693b28b81f4fb001aaad82
AffirmedVisionary <iam@charlenegrant.co.uk>
2020-11-09 12:56:32 +0000

    moved the build folder

---
+++ build/precache-manifest.b48f8c305ff4ef6511dd0a681fa4f5c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72a795b7b891d58916612058b862ab3",
+    "url": "/blackcodher-react-mybookcase2/index.html"
+  },
+  {
+    "revision": "7e2fd7b6156adae8b61e",
+    "url": "/blackcodher-react-mybookcase2/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "58cdd527029f45bea7d1",
+    "url": "/blackcodher-react-mybookcase2/static/css/main.c76e2deb.chunk.css"
+  },
+  {
+    "revision": "7e2fd7b6156adae8b61e",
+    "url": "/blackcodher-react-mybookcase2/static/js/2.9385b7f6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/blackcodher-react-mybookcase2/static/js/2.9385b7f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58cdd527029f45bea7d1",
+    "url": "/blackcodher-react-mybookcase2/static/js/main.c38117fd.chunk.js"
+  },
+  {
+    "revision": "d24be0c08303f8cd9285",
+    "url": "/blackcodher-react-mybookcase2/static/js/runtime-main.01888d37.js"
+  }
+]);
